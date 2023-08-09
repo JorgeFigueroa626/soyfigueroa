4c525d48bfb653bc46b0b66415be04e9903cf4d9
JorgeFigueroa626 <jlfigueroa626@gmail.com>
2023-08-08 23:32:36 -0600

    version v6

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ function efectoHabilidades() {
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
         document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
+        //document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("js").classList.add("barra-progreso4");
         document.getElementById("bd").classList.add("barra-progreso3");
         document.getElementById("ps").classList.add("barra-progreso4");
         document.getElementById("fr").classList.add("barra-progreso4");
